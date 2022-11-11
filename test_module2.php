f9a879eaf5da3522dc188f1a0894f6d5ab0fa4c7
Дмитрий Константинович Холостов <dkh@ro.ru>
2022-11-11 13:29:51 +0300

    v1.11

--- test_module2.php
+++ test_module2.php
@@ -1,3 +1,3 @@
 <?php
-	echo '<div>[test_module2 v1: connected]</div>';
+	echo '<div style="padding:20px;background:#ddd;border-radius:20px;margin:10px;font-family:sans-serif;">[test_module2 v1.1: connected]</div>';
 ?>
